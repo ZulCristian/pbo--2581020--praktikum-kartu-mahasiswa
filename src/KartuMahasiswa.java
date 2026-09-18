915d9847648b199a847c61817552b068093154df
Zul Cristian Purba <2581020@unai.edu>
2026-09-18 10:57:13 +0700

    menambahkan tampilan output

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -13,6 +13,28 @@ public class KartuMahasiswa {
         System.out.printf("Tinggi badan cm : ");
         Double Tinggi = input.nextDouble();
 
+        System.out.print("Golongan darah  : ");
+        char golongandarah = input.next().charAt(0);
+
+        System.out.print("Sudah Menikah  : ");
+        boolean sudahMenikah = input.nextBoolean();
+
+        input.nextLine();
+
+        System.out.print("Nama lengkap    : ");
+        String namaLengkap = input.nextLine();
+
+        System.out.println();
+        System.out.println("===== KARTU MAHASISWA =====");
+        System.out.println("Nama      : " + namaLengkap);
+        System.out.println("NIK       : " + NIK);
+        System.out.println("Umur      : " + Umur + " tahun");
+        System.out.println("Tinggi    : " + Tinggi + " cm");
+        System.out.println("Gol darah : " + golongandarah);
+        System.out.println("Menikah   : " + sudahMenikah);
+
+        input.close();
+
 
 
 
