8613ed560dd294b3d2fedbde73a7b0339b33f604
Zul Cristian Purba <2581020@unai.edu>
2026-09-18 09:59:07 +0700

    menambahkan NIK

---
+++ src/KartuMahasiswa.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class KartuMahasiswa {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("NIK (16 digit) : ");
+        String NIK = input.nextLine();
+
+
+
+
+
+        }
+    }
